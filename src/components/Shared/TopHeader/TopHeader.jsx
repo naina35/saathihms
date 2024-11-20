4c18ab65cfee5b82f4c55948441d3b1ee4771b08
PRABH <kprabhpreet25@gmail.com>
2024-11-20 10:09:37 +0530

    first commit

---
+++ src/components/Shared/TopHeader/TopHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaGithubSquare, FaPhoneAlt, FaEnvelope  } from "react-icons/fa";
+
+const TopHeader = () => {
+    return (
+        <div id="topbar" className="d-flex align-items-center fixed-top">
+            <div className="container d-flex justify-content-between">
+                <div className="contact-info d-flex align-items-center">
+                    
+                </div>
+                <div className="d-none d-lg-flex social-links align-items-center">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+export default TopHeader;
